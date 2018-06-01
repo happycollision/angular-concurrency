a56188fb29208d374ea627a7ecaadc3e5e83dd79
Don Denton <don@happycollision.com>
2018-06-01 15:36:42 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,33 +1,126 @@
-# Happy Javascript Helpers
+# Angular Concurrecny
 
-I have found tons of use for all these functions. If I don't ever find time to document them here, just have a look at the `.spec.js` file.
+Most everyone using Ember.js today loves [Ember Concurrency](http://ember-concurrency.com). But some of us unlucky souls don't get to choose the frameworks we must use at our jobs... so this project allows *similar* functionality in Angular. I am essentially adding functionality as I need it.
+
+I am using this code in production on a couple projects, but it is still a work in progress. See the examples below for the things you get out of the box.
+
+Because of Typescript limitations, there are *two* ways that you can create a task. One is simpler (and uses a decorator), and works fine if you only reference the task inside your HTML template. The other is more verbose, but will type-check properly if you refer to the task inside your class (if you invoke it from another function or something). I believe a future version of TS (after decorators are stage 4 in JS) will allow me to use only the simple version. I am not sure though.
+
+There are no plans (yet) to make this work with Observables. You'll need to only yield POJOs and Promises inside your generator.
+
+## Features
+
+- Automatically cancels all tasks when a component is destroyed
+- Schedule tasks' concurrency types using 'concurrent' (default), 'drop', and 'restart'
+- Access derived state of your tasks: `isRunning`, `lastCompletedValue`, `lastSuccessfulValue`, `lastErrorValue`, `currentValue`
+
+## Documentation
+
+PRs welcome. :wink:
+
+Love to you all!
+
+## Roadmap
+
+I am not necessarily interested in feature-pairity with EC. Feature requests are welcome. I will, however, eventually be sure that any terminology is comparable, but I have not taken the time to do that just yet.
+
+Things I would like to do:
+- Add tests (this will happen before any significant changes to the code)
+- Have documentation
+- Have live examples inside an actual Anguar app.
+
+## example:
+
+```ts
+import { Component, OnInit } from '@angular/core';
+import { createTask, Task, Schedule, TaskObject, timeout } from '../../lib/tasks';
+
+@Component({
+  selector: 'app-concurrency-test',
+  templateUrl: './concurrency-test.component.html',
+  styleUrls: ['./concurrency-test.component.scss'],
+})
+export class NewPackageComponent implements OnInit {
+  basicTaskText = 'nothing';
+  droppingTaskText = 'nothing';
+  cancelTaskSeconds = 0;
+  restartTaskSeconds = 0;
+
+  basicTask = createTask.call(this, function* () {
+    this.basicTaskText = 'waiting.';
+    yield timeout(500);
+    this.basicTaskText = 'waiting..';
+    yield timeout(500);
+    this.basicTaskText = 'waiting...';
+    yield timeout(500);
+    return this.basicTaskText = 'BLAMMO';
+  });
+
+
+  @Task('drop')
+  droppingTask = function* (this: NewPackageComponent) {
+    this.droppingTaskText = 'waiting.';
+    yield timeout(500);
+    this.droppingTaskText = 'waiting..';
+    yield timeout(500);
+    this.droppingTaskText = 'waiting...';
+    yield timeout(500);
+    return this.droppingTaskText = 'BLAMMO';
+  };
+
+
+  @Task({
+    onChange: function () { console.log('changed', this); },
+  })
+  cancelTask = function* (this: NewPackageComponent) {
+    let i = 0;
+    while (true) {
+      this.cancelTaskSeconds = ++i;
+      console.log('ran');
+      yield timeout(1000);
+    }
+  };
+
+  @Task('restart') restartTask: TaskObject = function* (this: NewPackageComponent) {
+    let i = 0;
+    while (true) {
+      this.restartTaskSeconds = ++i;
+      console.log('is running', this.restartTask.isRunning);
+      yield timeout(1000);
+    }
+  };
+}
+```
+
+```html
+<h3>unrestricted (concurrent)</h3>
+<div>is running: {{basicTask.isRunning}}</div>
+<div>count: {{basicTask.count}}</div>
+<div>{{basicTaskText}}</div>
+<button (click)="basicTask.perform()">Perform</button>
+
+<h3>cancel example</h3>
+<div>is running: {{cancelTask.isRunning}}</div>
+<div>count: {{cancelTask.count}}</div>
+<div>{{cancelTaskSeconds}}</div>
+<button (click)="cancelTask.perform()">Perform</button>
+<button (click)="cancelTask.cancelAll()">Cancel</button>
+
+<h3>droppable</h3>
+<div>is running: {{droppingTask.isRunning}}</div>
+<div>count: {{droppingTask.count}}</div>
+<div>{{droppingTaskText}}</div>
+<button (click)="droppingTask.perform()" [disabled]="droppingTask.isRunning">Perform</button>
+
+<h3>restartable example</h3>
+<div>is running: {{restartTask.isRunning}}</div>
+<div>count: {{restartTask.count}}</div>
+<div>{{restartTaskSeconds}}</div>
+<button (click)="restartTask.perform()">Perform</button>
+<button (click)="restartTask.cancelAll()">Cancel</button>
+```
 
 # Changelog
-
-### 1.2.2
-* `nestedPropertyDetails` now properly handles circular objects
-
-### 1.2.1
-* update package to export types properly
-
-## 1.2.0
-* Adds Typescript types
-* fixes a couple bugs caught by Typescript
-
-### 1.1.1
-* updates this changelog to add 1.1.0. Oops.
-
-## 1.1.0
-* adds a `stringify` function.
-  > The stringify function creates a nicely indented JSON string of any object. Even those with circular references.
-  >
-  > You can optionally strip out the quotes around properties and change the indent amount.
-
-### 1.0.2
-* updates `package.json` with the correct repository information. oops.
-
-### 1.0.1
-* fix for a bug in IE when using the `isEmpty` function with a string as the arg
 
 ## 1.0.0
 * inital release
---
+++ src/task.ts
@@ -0,0 +1,238 @@
+import { Component } from '@angular/compiler/src/core';
+import { ChangeDetectorRef } from '@angular/core/src/change_detection';
+
+export async function timeout(time: number = 0) {
+  return new Promise(res => setTimeout(res, time));
+}
+
+// See https://basarat.gitbooks.io/typescript/content/docs/types/literal-types.html
+function strEnum<T extends string>(o: Array<T>): { [K in T]: K } {
+  return o.reduce((res, key) => {
+    res[key] = key;
+    return res;
+  }, Object.create(null));
+}
+
+export const Schedule = strEnum([
+  'concurrent',
+  'drop',
+  'restart',
+]);
+
+export type Schedule = keyof typeof Schedule;
+
+export interface TaskOptions {
+  schedule?: Schedule;
+  onChange?: (task: TaskObject) => void;
+}
+
+let uid = 0;
+function getNewId(): number {
+  return ++uid;
+}
+
+export class TaskInstance {
+  id = getNewId();
+  hasStarted = false;
+  isRunning = false;
+  isCancelled = false;
+  completed: { success: boolean, error: boolean, value: any } = null;
+
+  private get shouldRun(): boolean {
+    return (!this.isCancelled) && this.isRunning;
+  }
+
+  constructor(
+    private generatorFn: GeneratorFunction,
+    private onCompleteFn: (completedTaskInstance: TaskInstance) => any,
+  ) { }
+
+  public cancel() {
+    this.endWithCancel();
+  }
+
+  public start(context: object): this {
+    if (this.hasStarted) return this;
+    this.hasStarted = true;
+    this.isRunning = true;
+    const iterator = this.generatorFn.call(context);
+    this.run(context, iterator);
+    return this;
+  }
+
+  private async run(context: object, iterator: Generator, lastValue?: any) {
+    if (this.shouldRun) {
+      let resolved;
+      const { value, done } = iterator.next.call(context, lastValue);
+      try {
+        resolved = await value;
+      } catch (e) {
+        return this.endWithErrorValue(e);
+      }
+      if (done) {
+        this.endWithSuccessValue(resolved);
+      } else {
+        this.run(context, iterator, resolved);
+      }
+    }
+  }
+
+  private end() {
+    this.isRunning = false;
+    this.onCompleteFn(this);
+  }
+
+  private endWithSuccessValue(value: any) {
+    this.completed = { success: true, error: false, value };
+    this.end();
+  }
+
+  private endWithCancel() {
+    this.isCancelled = true;
+    this.end();
+  }
+
+  private endWithErrorValue(error: Error) {
+    this.completed = { success: false, error: true, value: error };
+    this.end();
+  }
+}
+
+export class TaskObject {
+  private instances: TaskInstance[] = [];
+  private parentDestroyed = false;
+  private _lastCompletedValue: any = null;
+  private _lastSuccessfulValue: any = null;
+  private _lastErrorValue: Error = null;
+  private _currentValue: any;
+  private _schedule: Schedule = Schedule.concurrent;
+  private _onChange: (task: TaskObject) => void = () => { };
+
+  public get lastCompletedValue(): TaskObject['_lastCompletedValue'] { return this._lastCompletedValue; }
+  public get lastSuccessfulValue(): TaskObject['_lastSuccessfulValue'] { return this._lastSuccessfulValue; }
+  public get lastErrorValue(): TaskObject['_lastErrorValue'] { return this._lastErrorValue; }
+  public get currentValue(): TaskObject['_currentValue'] { return this._currentValue; }
+  public get schedule(): Schedule { return this._schedule; }
+
+  get isRunning(): boolean {
+    return this.instances.length > 0 && this.instances.some(instance => instance.isRunning);
+  }
+
+  get count(): number {
+    return this.instances.filter(instance => instance.isRunning).length;
+  }
+
+  constructor(
+    private parentComponent: Component,
+    private generatorFn: GeneratorFunction,
+    private changeDetector?: ChangeDetectorRef,
+  ) {
+    this.hookUpParentEvents();
+  }
+
+  public setSchedule(schedule?: Schedule): this {
+    if (schedule) { this._schedule = schedule; }
+    return this;
+  }
+
+  public perform(): TaskInstance {
+    if (this.parentDestroyed) return this.dropNewInstance();
+
+    switch (this.schedule) {
+      case Schedule.concurrent:
+        return this.startNewInstance();
+      case Schedule.drop:
+        if (this.isRunning) {
+          return this.dropNewInstance();
+        } else {
+          return this.startNewInstance();
+        }
+      case Schedule.restart:
+        this.cancelAll();
+        return this.startNewInstance();
+    }
+  }
+
+  public cancelAll() {
+    this.instances.forEach(inst => inst.cancel());
+  }
+
+  public onChange(fn?: (task: TaskObject) => void): this {
+    if (fn) { this._onChange = fn; }
+    return this;
+  }
+
+  private hookUpParentEvents() {
+    const component = this.parentComponent;
+    const parentDestroy: () => void = component['ngOnDestroy'] || (() => { });
+
+    component['ngOnDestroy'] = () => {
+      this._onChange = () => { };
+      this.cancelAll();
+      this.parentDestroyed = true;
+      parentDestroy.call(component);
+    };
+  }
+
+  private startNewInstance(): TaskInstance {
+    this._currentValue = null;
+    const newInstance = new TaskInstance(this.generatorFn, this.evaluateReturn.bind(this)).start(this.parentComponent);
+    this.instances.push(newInstance);
+    this.signalChange();
+    return newInstance;
+  }
+
+  private dropNewInstance(): TaskInstance {
+    const newInstance = new TaskInstance(this.generatorFn, this.evaluateReturn.bind(this));
+    newInstance.cancel();
+    this.instances.push(newInstance);
+    this.signalChange();
+    return newInstance;
+  }
+
+  private evaluateReturn(finishedTask: TaskInstance) {
+    this.signalChange();
+    if (finishedTask.isCancelled) { return; }
+    if (finishedTask.completed.success) {
+      this._lastSuccessfulValue = finishedTask.completed.value;
+    }
+    if (finishedTask.completed.error) {
+      this._lastErrorValue = finishedTask.completed.value;
+      this._lastCompletedValue = finishedTask.completed.value;
+    }
+    this._currentValue = finishedTask.completed.value;
+  }
+
+  private signalChange() {
+    this._onChange(this);
+  }
+}
+
+export function createTask(this: Component, generator: GeneratorFunction): TaskObject {
+  return new TaskObject(this, generator);
+}
+
+
+function taskDecoratorFactory(options: TaskOptions | Schedule = {}) {
+  const settings: TaskOptions = typeof options === 'string' ? { schedule: options } : options;
+  return function taskDecorator(target: any, propKey: string) {
+    if (!target['ngOnDestroy']) { target['ngOnDestroy'] = () => { }; }
+    const oldInit = target['ngOnInit'] || (() => { });
+
+    target['ngOnInit'] = function () {
+      // `this` ends up being the instance
+      let theTask: TaskObject;
+      if (this[propKey] instanceof TaskObject) {
+        theTask = this[propKey];
+      } else {
+        theTask = createTask.call(this, this[propKey]);
+      }
+      this[propKey] = theTask
+        .onChange(settings.onChange)
+        .setSchedule(settings.schedule);
+      oldInit.call(this);
+    };
+  };
+}
+
+export { taskDecoratorFactory as Task };
