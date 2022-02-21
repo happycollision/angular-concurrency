8e39029e9224000070e83e848f373aac83205428
Philipp Bucher <bucher@netzreich.de>
2022-02-21 15:59:28 +0100

    Fix TypeError: Method [Generator].prototype.next called on incompatible receiver

--- src/task.ts
+++ src/task.ts
@@ -84,7 +84,7 @@ export class TaskInstance implements ITaskInstance {
   private async run(context: object, iterator: Generator, lastValue?: any) {
     if (this.shouldRun) {
       let resolved;
-      const { value: valueToCheck, done } = iterator.next.call(context, lastValue);
+      const { value: valueToCheck, done } = iterator.next.bind(iterator).call(context, lastValue);
       const value = this.inspectForSpecialYields(valueToCheck);
       try {
         resolved = await value;
